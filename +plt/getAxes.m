d987ab534ca8d6f99612cae32a3f9dbdff6261d1
pipcoen <pipcoen@gmail.com>
2020-03-27 19:24:24 +0000

    Update

--- +plt/getAxes.m
+++ +plt/getAxes.m
@@ -1,22 +1,40 @@
 function [axesHandle, figureHandle] = getAxes(axesOpt, idx)
+%% A method for to create and/or select figure and axes for an upcoming plot based on some
+% INPUTS(default values)
+% axesOpt('res')--------String indicating the type of data to plot. Options are
+%	.totalNumOfAxes(1)-----------------The total number of axes in the figure
+%	.figureSize([400 400])-------------Size of the figure in pixels [width height]
+%	.gapBetweenAxes(25)----------------Space (in pixels) between the axes
+%	.btlrMargins([50 50 50 50])--------The total number of axes in the figure
+%	.reposition(1)---------------------Whether to position figure at the top-right of the left-most screen
+%	.numOfRows([])---------------------Specify the number of rows to use (for figures with multiple axes)
+
+% OUTPUTS
+% axesHandle-------------handle for genereated axis
+% figureHandle-----------handle for the figure created/used
+
+%% Set defaults etc.
+%Set detault values
 if ~exist('idx', 'var'); idx = 1; end
 if ~isfield(axesOpt, 'totalNumOfAxes'); axesOpt.totalNumOfAxes = 1; end
-if ~isfield(axesOpt, 'figureSize'); axesOpt.figureSize = 400; end
-if ~isfield(axesOpt, 'figureHWRatio'); axesOpt.figureHWRatio = 1; end
+if ~isfield(axesOpt, 'figureSize'); axesOpt.figureSize = [400 400]; end
 if ~isfield(axesOpt, 'gapBetweenAxes'); axesOpt.gapBetweenAxes = 25; end
 if ~isfield(axesOpt, 'btlrMargins'); axesOpt.btlrMargins = 50*ones(1,4); end
 if ~isfield(axesOpt, 'reposition'); axesOpt.reposition = 1; end
+if ~isfield(axesOpt, 'numOfRows'); axesOpt.numOfRows = []; end
 
+%Detect the screensize and screen ratio (width/height)
 screenSize = get(0,'MonitorPositions');
 screenSize = screenSize(screenSize(:,1)==min(screenSize(:,1)),:);
 screenRatio = round(screenSize(3)/screenSize(4));
 
-if axesOpt.totalNumOfAxes < 4; numOfRows = 1;
-elseif isfield(axesOpt, 'numOfRows'); numOfRows = axesOpt.numOfRows;
-else, numOfRows = find(((1:5)*screenRatio.*(1:5))>=axesOpt.totalNumOfAxes,1);
+if ~isempty(axesOpt.numOfRows); numOfRows = axesOpt.numOfRows;
+elseif axesOpt.totalNumOfAxes < 4; numOfRows = 1;
+else, numOfRows = find(4*screenRatio.*(1:100)>=axesOpt.totalNumOfAxes,1);
 end
+
 numOfCols = ceil(axesOpt.totalNumOfAxes/numOfRows);
-axesOpt.figureSize = min([axesOpt.figureSize*numOfCols*axesOpt.figureHWRatio, axesOpt.figureSize*numOfRows], screenSize(3:4));
+axesOpt.figureSize = min([axesOpt.figureSize(1)*numOfCols, axesOpt.figureSize(2)*numOfRows], screenSize(3:4));
 
 axesOpt.btlrMargins(1:2) = axesOpt.btlrMargins(1:2)/axesOpt.figureSize(2);
 axesOpt.btlrMargins(3:4) = axesOpt.btlrMargins(3:4)/axesOpt.figureSize(1);
